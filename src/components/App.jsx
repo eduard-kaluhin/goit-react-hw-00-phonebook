e6e4052e89e59c1ce5eb31973f57b576e65d7b45
eduard-kaluhin <renoserviser@gmail.com>
2023-08-26 14:47:42 +0300

    con

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,17 @@
-import { Div } from './App.styled';
-
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
+import { Div } from './App.styled';
 
 const App = () => {
   return (
-    <div>
+    <Div>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
-    </div>
+    </Div>
   );
 };
 
